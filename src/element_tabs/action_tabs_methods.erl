5e2695930a99f76fc78bd56805cc02b35f9606d6
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-11-06 21:09:58 +0000

    some modifications to tabs

--- src/element_tabs/action_tabs_methods.erl
+++ src/element_tabs/action_tabs_methods.erl
@@ -6,49 +6,56 @@
 -include("nitrogen_elements.hrl").
 -compile(export_all).
 
-render_action(_TriggerPath, TargetPath, Record) ->
-    Script = case element(1, Record) of
-		 tab_destroy -> "'destroy'";
-		 tab_disable ->
-		     "'disable'" ++
-			 case Record#tab_disable.tab of
-			     -1 -> "";
-			     Idx -> wf:f(", ~w", [Idx])
-			 end;
-		 tab_enable ->
-		     "'enable'" ++
-			 case Record#tab_enable.tab of
-			     -1 -> "";
-			     Idx -> wf:f(", ~w", [Idx])
-			 end;
-		 tab_option ->
-		     wf:f("'option', '~s', ~s",
-			  [Record#tab_option.key,
-			   value_to_js(Record#tab_option.value)]);
-		 tab_add ->
-		     wf:f("'add', '~s', '~s'",
-			  [Record#tab_add.url, Record#tab_add.label])
-			 ++
-			 case Record#tab_add.index of
-			     undefined -> "";
-			     Idx -> wf:f(", ~w", [Idx])
-			 end;
-		 tab_remove ->
-		     wf:f("'remove', ~w", [Record#tab_remove.tab]);
-		 tab_select ->
-		     wf:f("'select', ~w", [Record#tab_select.tab]);
-		 tab_load -> wf:f("'load', ~w", [Record#tab_load.tab]);
-		 tab_url ->
-		     wf:f("'url', ~w, '~s'",
-			  [Record#tab_url.tab, Record#tab_url.url]);
-		 tab_abort -> "'abort'";
-		 tab_rotate ->
-		     wf:f("'rotate', ~w, ~s",
-			  [Record#tab_rotate.ms, Record#tab_rotate.continuing])
-	     end,
-    [wf:me_var(),
-     wf:f("jQuery(obj('~s')).tabs(~s);",
-	  [wf:to_js_id(TargetPath), Script])].
+%% render_action(Record) ->
+%%     {Script, Target} =
+%% 	case element(1, Record) of
+%% 	    tab_destroy -> {"'destroy'", Record#tab_destroy.target};
+%% 	    tab_disable ->
+%% 		{"'disable'" ++
+%% 		     case Record#tab_disable.tab of
+%% 			 -1 -> "";
+%% 			 Idx -> wf:f(", ~w", [Idx])
+%% 		     end,
+%% 		 Record#tab_disable.target};
+%% 	    tab_enable ->
+%% 		{"'enable'" ++
+%% 		     case Record#tab_enable.tab of
+%% 			 -1 -> "";
+%% 			 Idx -> wf:f(", ~w", [Idx])
+%% 		     end,
+%% 		 Record#tab_enable.target};
+%% 	    tab_option -> {wf:f("'option', '~s', ~s", [Record#tab_option.key, value_to_js(Record#tab_option.value)]),
+%% 			   Record#tab_option.target};
+%% 	    tab_add ->
+%% 		{wf:f("'add', '~s', '~s'", [Record#tab_add.url, Record#tab_add.label])
+%% 		 ++
+%% 		     case Record#tab_add.index of
+%% 			 undefined -> "";
+%% 			 Idx -> wf:f(", ~w", [Idx])
+%% 		     end,
+%% 		 Record#tab_add.target};
+%% 	    tab_remove -> {wf:f("'remove', ~w", [Record#tab_remove.tab]), Record#tab_remove.target};
+%% 	    tab_select -> {wf:f("'select', ~w", [Record#tab_select.tab]), Record#tab_select.target};
+%% 	    tab_load -> {wf:f("'load', ~w", [Record#tab_load.tab]), Record#tab_load.target};
+%% 	    tab_url -> {wf:f("'url', ~w, '~s'", [Record#tab_url.tab, Record#tab_url.url]), Record#tab_url.target};
+%% 	    tab_abort -> {"'abort'", Record#tab_abort.target};
+%% 	    tab_rotate ->{wf:f("'rotate', ~w, ~s", [Record#tab_rotate.ms, Record#tab_rotate.continuing]), Record#tab_rotate.target}
+%% 	end,
+%%     wf:f("jQuery(obj('~s')).tabs(~s);",  [wf:to_js_id(Target), Script]).
+
+render_action(#tab_destroy{target = Target}) ->
+    wf:f("jQuery(obj('~s')).tabs('destroy');", [wf:to_js_id(Target)]);
+render_action(#tab_disable{target = Target, tab = Index}) ->
+    wf:f("jQuery(obj('~s')).tabs('disable', '~s');", [wf:to_js_id(Target), Index]);
+render_action(#tab_enable{target = Target, tab = Index}) ->
+    wf:f("jQuery(obj('~s')).tabs('enable;, '~s');", [wf:to_js_id(Target), Index]);
+render_action(#tab_add{target = Target, url = Url, label = Label}) ->
+    wf:f("jQuery(obj('~s')).tabs('add', '~s', '~s');", [wf:to_js_id(Target), Url, Label]);
+render_action(#tab_remove{target = Target, tab = Index}) ->
+    wf:f("jQuery(obj('~s')).tabs('remove', '~s');", [wf:to_js_id(Target), Index]);
+render_action(#tab_select{target = Target, tab = Index}) ->
+    wf:f("jQuery(obj('~s')).tabs('select', ~w);", [Target, Index]).
+
 
 value_to_js(Value) when is_list(Value) ->
     wf:f("'~s'", [wf_utils:js_escape(Value)]);
--- src/element_tabs/element_tabs.erl
+++ src/element_tabs/element_tabs.erl
@@ -4,75 +4,35 @@
 
 -module (element_tabs).
 -include ("nitrogen_elements.hrl").
+-include_lib("nitrogen_core/include/wf.hrl").
 -compile(export_all).
 
 reflect() -> record_info(fields, tabs).
 
-render(ControlID, Record) -> 
-    PickledPostBackInfo = case Record#tabs.tag of
-			      undefined -> "false";
-			      Tag ->
-				  [wf:wire(wf:f(
-					    "Nitrogen.$tab(obj('~s'), '~s')",
-					     [R#tab.id, wf_utils:pickle(R#tab.tag)]))
-				   || R <- [#tab{ id=ControlID, tag=Record#tabs.tag }
-					    |Record#tabs.tabs], R#tab.tag /= undefined],
-				  
-				  wf:f("'~s'",
-				       [action_event:make_postback_info(
-					  Tag, tabsevent,
-					  ControlID, ControlID,
-					  ?MODULE)
-				       ])
-			  end,
-    
-    Options = action_jquery_effect:options_to_js(
-		Record#tabs.options),
-    
-    Script = wf:f("Nitrogen.$tabs(obj('~s'), ~s, ~s)",
-		  [ControlID, Options, PickledPostBackInfo]),
 
-    wf:wire(Script),
-    
-    Terms = #panel{
-      class = "tabs " ++ wf:to_list(Record#tabs.class),
-      style = Record#tabs.style,
-      body = [
-	      #list{ body=[
-			   #listitem{ body=tab_link(Tab) }
-			   || Tab <- Record#tabs.tabs ] },
-	      [#panel{ id = Tab#tab.id,
-		       class = "tab",
-		       body = Tab#tab.body } 
-	       || Tab <- Record#tabs.tabs]
-	     ]
-     },
-    
-    element_panel:render(ControlID, Terms).
+render_element(Record) ->
+    TabsID =
+	case Record#tabs.id of
+	    undefined -> wf:temp_id();
+	    Other -> Other
+	end,
 
-event(TabsTag) ->
-    [EventStr] = wf:q(event),
-    [IndexStr] = wf:q(tab_index),
-    [TabItem] = wf:q(tab_tag),
-    Event = list_to_atom(EventStr),
-    Index = list_to_integer(IndexStr),
-    TabTag = wf:depickle(TabItem),
-    Module = wf_platform:get_page_module(),
-    Module:tabs_event(Event, TabsTag, TabTag, Index).
+    Options = action_jquery_effect:options_to_js(Record#tabs.options),
+    wf:wire(TabsID, wf:f("jQuery(obj('~s')).tabs(~s)", [TabsID, Options])),
 
-html_id(Id) ->
-    case wf_path:is_temp_element(Id) of
-	true ->
-	    wf_path:to_html_id(Id);
-	false ->
-	    wf_path:push_path(Id),
-	    HtmlId = wf_path:to_html_id(wf_path:get_path()),
-	    wf_path:pop_path(),
-	    HtmlId
-    end.
+    #panel{
+	id = TabsID,
+	body = [
+	    #list{class = wf:to_list(Record#tabs.class),
+		body = [#listitem{body = tab_link(Tab)} || Tab <- Record#tabs.tabs]},
+	    [#panel{html_id = Tab#tab.id, body = Tab#tab.body} || Tab <- Record#tabs.tabs]
+	]
+    }.
 
-tab_link(#tab{ url=undefined, id=Id, title=Title }) ->
-    #link{ url="#" ++ html_id(Id), body=Title };
-tab_link(#tab{ url=Url, id=Id, title=Title }) ->
-    %% only needed until nitrogens #link supports the title attribute
-    #link_x{ url=Url, title=html_id(Id), body=Title }.
+tab_link(#tab{id = Id, title = Title}) when is_atom(Id) ->
+    #link{url = "#" ++ wf:html_encode(atom_to_list(Id)), body = Title};
+tab_link(#tab{id = Id, title = Title}) when is_list(Id) ->
+    #link{url = "#" ++ wf:html_encode(Id), body = Title}.
+
+event(Event) ->
+    ?PRINT({tabs_event, Event}).
