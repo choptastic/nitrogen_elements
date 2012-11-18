acd4785af09e805570760474a1c4511085e1a54c
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-11-18 17:57:10 +0000

    init changes to enable back button action with jQuery BBQ

--- src/element_tabs/element_tabs.erl
+++ src/element_tabs/element_tabs.erl
@@ -17,23 +17,61 @@ render_element(Record) ->
 	end,
 
     %% init jQuery tabs control with specified options
-    Options = action_jquery_effect:options_to_js(Record#tabs.options),
-    wf:wire(TabsID, wf:f("jQuery(obj('~s')).tabs(~s)", [TabsID, Options])),
+    %% Options = action_jquery_effect:options_to_js(Record#tabs.options),
+    %% wf:wire(TabsID, wf:f("jQuery(obj('~s')).tabs(~s)", [TabsID, Options])),
 
-    %% wire standard tabs events to the element, instance of tabs element needs to be tagged to receive events
-    case Record#tabs.tag of
-    	underfined -> skip;
-    	TabsTag ->
-    	    PickledPostbackInfo = wf_event:serialize_event_context(tabsevent, TabsID, TabsID, ?MODULE),
-    	    [wf:wire(TabsID, wf:f("jQuery(obj('~s')).bind('~s', function(e, ui) {
-    		Nitrogen.$queue_event('~s','~s',\"event=\" + e.type + \"&tabs_tag=\" + '~s' + \"&tab=\" + ui.tab + \
-    		\"&panel=\" + ui.panel + \"&index=\" + ui.index)})",
-    		[TabsID, Event, TabsID, PickledPostbackInfo, TabsTag])) ||
-    		Event <- ['tabsselect', 'tabsload', 'tabsshow', 'tabsadd', 'tabsremove', 'tabsenable', 'tabsdisable']]
-    end,
+    wf:wire(wf:f("$(function() {
+	var tabs = jQuery(obj('~s')),
+	tab_a_selector = 'ul.ui-tabs-nav a';
+	tabs.tabs({ event: 'change' });
+	tabs.find( tab_a_selector ).click(function(){
+	var state = {},
+	id = $(this).closest( '~s' ).attr( 'id' ),
+	idx = $(this).parent().prevAll().length;
+	state[ id ] = idx;
+	$.bbq.pushState( state );
+	});
+	$(window).bind( 'hashchange', function(e) {
+	tabs.each(function() {
+	var idx = $.bbq.getState(this.id, true) || 0;
+	$(this).find( tab_a_selector ).eq(idx).triggerHandler( 'change' );
+        }); })
+	$(window).trigger( 'hashchange' ); })",
+	[TabsID, TabsID])),
+
+    %% %% wire standard tabs events to the element, instance of tabs element needs to be tagged to receive events
+    %% case Record#tabs.tag of
+    %% 	underfined -> skip;
+    %% 	TabsTag ->
+    %% 	    PickledPostbackInfo = wf_event:serialize_event_context(tabsevent, TabsID, TabsID, ?MODULE),
+    %% 	    [wf:wire(TabsID, wf:f("jQuery(obj('~s')).bind('~s', function(e, ui) {
+    %% 		Nitrogen.$queue_event('~s','~s',\"event=\" + e.type + \"&tabs_tag=\" + '~s' + \"&tab=\" + ui.tab + \
+    %% 		\"&panel=\" + ui.panel + \"&index=\" + ui.index)})",
+    %% 		[TabsID, Event, TabsID, PickledPostbackInfo, TabsTag])) ||
+    %% 		Event <- ['tabsselect', 'tabsload', 'tabsshow', 'tabsadd', 'tabsremove', 'tabsenable', 'tabsdisable']]
+    %% end,
+
+                %% window.location.href = window.location.protocol + '//' + window.location.hostname + \
+                %% window.location.pathname + ui.tab.hash;
+
+
+    %% %% wire standard tabs events to the element, instance of tabs element needs to be tagged to receive events
+    %% case Record#tabs.tag of
+    %% 	underfined -> skip;
+    %% 	TabsTag ->
+    %% 	    PickledPostbackInfo = wf_event:serialize_event_context(tabsevent, TabsID, TabsID, ?MODULE),
+    %% 	    wf:wire(TabsID, wf:f("jQuery(obj('~s')).bind('~s', function(e, ui) { \
+    %%             window.location.href = window.location.protocol + '//' + window.location.hostname + \
+    %%             window.location.pathname + ui.tab.hash;
+    %% 		Nitrogen.$queue_event('~s','~s',\"event=\" + e.type + \"&tabs_tag=\" + '~s' + \"&tab=\" + ui.tab + \
+    %% 		\"&panel=\" + ui.panel + \"&index=\" + ui.index)})",
+    %% 		[TabsID, 'tabselect', TabsID, PickledPostbackInfo, TabsTag]))
+    %% 		%Event <- ['tabsselect', 'tabsload', 'tabsshow', 'tabsadd', 'tabsremove', 'tabsenable', 'tabsdisable']]
+    %% end,
 
     #panel{
 	id = TabsID,
+	html_id = "tabs1",
 	body = [
 	    #list{
 		class = wf:to_list(Record#tabs.class),
@@ -51,11 +89,11 @@ tab_link(#tab{url=Url, id=Id, title=Title}) ->
     #link{url=Url, title = wf:html_encode(Id), body=Title}.
 
 event(Event) ->
-    ?PRINT({tabsevent, Event}),
-    EventType = wf:q(event),
-    TabsTag = wf:q(tabs_tag),
-    TabAnchor = wf:q(tab),
-    TabPanel = wf:q(panel),
-    TabIndex = wf:q(index),
-    Module = wf:page_module(),
-    Module:tabs_event(EventType, TabsTag, TabAnchor, TabPanel, TabIndex).
+    ?PRINT({tabsevent, Event}).
+    %% EventType = wf:q(event),
+    %% TabsTag = wf:q(tabs_tag),
+    %% TabAnchor = wf:q(tab),
+    %% TabPanel = wf:q(panel),
+    %% TabIndex = wf:q(index),
+    %% Module = wf:page_module(),
+    %% Module:tabs_event(EventType, TabsTag, TabAnchor, TabPanel, TabIndex).
