334ed495abe07d716d923dddb4c9a2fda4bb702e
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-12-11 22:58:29 +0000

    Revert "init changes to enable back button action with jQuery BBQ"
    This reverts commit acd4785af09e805570760474a1c4511085e1a54c.

--- src/misc/element_icon.erl
+++ src/misc/element_icon.erl
@@ -6,25 +6,23 @@
 
 reflect() -> record_info(fields, icon).
 
-render_element(Record) ->
-    Item = lists:concat(["icon-", Record#icon.type]),
-    Group = lists:concat(["container-", Record#icon.kind]),
-    Icon =
-    #tablecell {
-	style = "border: 0;",
-	body = #panel{class=Group, body=#link{class=Item, url=Record#icon.url}}
-    },
-
-    Body =
-    case Record#icon.body of
-	[] -> [];
-	_ -> #tablecell{body = Record#icon.body}
-    end,
-
-    #singlerow
-    {
-	id = Record#icon.id,
-	class = Record#icon.class,
-	style = Record#icon.style,
-	cells = [Icon, Body]
-    }.
+render(ControlID, Record) ->
+	Item = lists:concat(["icon-", Record#icon.type]),
+	Group = lists:concat(["container-", Record#icon.kind]),
+	Icon = #tablecell{
+		style = "border: 0;",
+		body = #panel{class=Group, body=#link{class=Item, url=Record#icon.url}}
+	},
+	
+	case Record#icon.body of
+		[] -> Body = [];
+		_ -> Body = #tablecell{body = Record#icon.body}
+	end,
+	
+	Obj = #singlerow{
+		id = Record#icon.id,
+		class = Record#icon.class,
+		style = Record#icon.style,
+		cells = [Icon, Body]
+	},
+    element_singlerow:render(ControlID, Obj).
